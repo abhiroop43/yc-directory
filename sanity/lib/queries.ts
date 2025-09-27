5a97979035e9aa94cc0f039b2b3072faabb67fe4
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-09-28 02:07:29 +0400

    feat: query sanity for startup details and apply ppr to render the details
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,3 +12,13 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup"
     }, 
     views, description, category, image
 }`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" 
+&& _id == $id][0] 
+ {
+  _id, title, slug, _createdAt, 
+    author -> {
+      _id, name, image, bio
+    }, 
+    views, description, category, image, pitch
+}`);
